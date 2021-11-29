210a6be995d5a2c752c3f818bcbaa7584c37bf28
Lucas <luke.dluz@gmail.com>
2021-11-29 17:12:45 -0300

    Initial commit

---
+++ sc4r.lua
@@ -0,0 +1,8 @@
+﻿function Sc4r()
+     local time = getRealTime()
+	 local hours = time.hour
+	 local minutes = time.minute
+	 setTime ( hours, minutes )
+	 setMinuteDuration(60000)
+end
+addEventHandler("onResourceStart", getRootElement(), Sc4r )
